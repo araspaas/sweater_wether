cc9f1a7f5dacdf4f834e240cc0f9035f378f5583
Austin Aspaas <araspaas@live.com>
2021-01-18 12:15:24 -0700

    Add forcast controller with index action

---
+++ app/controllers/api/v1/forecast_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::ForecastController < ApplicationController
+  def index
+    destination = Destination.new(params[:location])
+    forecast = Forecast.new(destination.latitude, destination.longitude).forecast
+  end
+end
