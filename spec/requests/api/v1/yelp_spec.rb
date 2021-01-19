858271718b0265ea1d87ecd2c311b3011a14fa4a
Austin Aspaas <araspaas@live.com>
2021-01-19 09:31:23 -0700

    Not true test. just to get started

---
+++ spec/requests/api/v1/yelp_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'Yelp Fusion API' do
+  it 'returns', :vcr do
+    get '/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese'
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(json[:data]).to be_a(Hash)
+    expect(json[:data][:attributes][:current][:weather]).to be_a(Array)
+    expect(json[:data][:attributes][:hourly]).to be_a(Array)
+    expect(json[:data][:attributes][:daily]).to be_a(Array)
+    expect(json[:data][:attributes][:daily].length).to eq(8)
+    expect(json[:data][:id]).to eq(nil)
+  end
+end
