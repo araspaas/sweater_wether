cc20d0dfc8fc681f803d5bf3385b2a494ef76136
Austin Aspaas <araspaas@live.com>
2021-01-19 19:37:05 -0700

    Refactor location service

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -1,14 +1,18 @@
 class LocationService
 
-  def get_location(destination)
+  def self.fetch_data(location)
     response = conn.get("geocoding/v1/address") do |req|
       req.params[:key] = ENV["MAPQUEST_API_KEY"]
-      req.params[:location] = destination
+      req.params[:location] = location
+      req.params[:inFormat] = 'kvp'
+      req.params[:outFormat] = 'json'
+      req.params[:thumbMaps] = 'false'
     end
-    JSON.parse(response.body, symbolize_names: true)
+    json = JSON.parse(response.body, symbolize_names: true)
+     x = [(json[:results][0][:locations][0][:latLng][:lat]).to_f, (json[:results][0][:locations][0][:latLng][:lng]).to_f]
   end
 
-  def conn
+  def self.conn
     Faraday.new("http://www.mapquestapi.com/")
   end
 end
