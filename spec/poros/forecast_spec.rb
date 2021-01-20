398ce09ada352334c2276720cf7460a72dae9d16
Austin Aspaas <araspaas@live.com>
2021-01-19 19:32:07 -0700

    Update forecast_spec to use new Poro. tests passing

--- spec/poros/forecast_spec.rb
+++ spec/poros/forecast_spec.rb
@@ -1,18 +1,50 @@
 require 'rails_helper'
 
-describe Forecast, :vcr do
-  it "exists" do
-    forecast = Forecast.new("denver,co", 39.738453, -104.984853)
-    expect(forecast).to be_a(Forecast)
-  end
+describe Forecast do
+  it 'exists' do
+    current_data = {
+      :current => {
+        dt: 1_610_910_000,
+        sunrise: 1_610_893_087,
+        sunset: 1_610_893_087,
+        temp: 47.8,
+        feels_like: 41.58,
+        humidity: 28,
+        uvi: 1.65,
+        visibility: 10_000,
+        weather: [{ description: 'windy', icon: '04g' }]
+      }
+    }
 
+    daily_data = {
+      :daily => [{
+        dt: 1_610_910_000,
+        sunrise: 1_610_893_087,
+        sunset: 1_610_928_108,
+        temp: { max: 90.0, min: 60.5 },
+        weather: [{ description: 'windy', icon: '04g' }]
 
-  it "attributes" do
-    forecast = Forecast.new("denver,co", 39.738453, -104.984853)
-    expect(forecast.location).to eq("denver,co")
-    expect(forecast.current).to be_a(Hash)
-    expect(forecast.hourly).to be_a(Array)
-    expect(forecast.daily).to be_a(Array)
-    expect(forecast.fake_id).to eq(nil)
+        }]
+    }
+
+    hourly_data = {
+      :hourly => [{
+        dt: 1_610_910_000,
+        temp: 47.21,
+        wind_speed: 1.72,
+        wind_deg: 173,
+        weather: [{ description: 'windy', icon: '04g' }]
+        }]
+    }
+
+    current_weather = CurrentWeather.new(current_data)
+    daily_weather = DailyWeather.new(daily_data)
+    hourly_weather = HourlyWeather.new(hourly_data)
+
+    weather = Forecast.new(current_weather, daily_weather, hourly_weather)
+    expect(weather).to be_a(Forecast)
+    expect(weather.current_weather).to be_a(CurrentWeather)
+    expect(weather.daily_weather).to be_a(DailyWeather)
+    expect(weather.hourly_weather).to be_a(HourlyWeather)
   end
 end
