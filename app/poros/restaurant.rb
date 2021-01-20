1e171557bbb12eb2c6ffba2ba2bd16dcc2a6dbed
Austin Aspaas <araspaas@live.com>
2021-01-20 11:38:03 -0700

    Remove extra data

--- app/poros/restaurant.rb
+++ app/poros/restaurant.rb
@@ -1,13 +1,10 @@
 class Restaurant
   attr_reader :name,
-              :address,
-              :latitude,
-              :longitude
+              :address
+
 
   def initialize(data)
     @name = data[:restaurants][0][:restaurant][:name]
     @address = data[:restaurants][0][:restaurant][:location][:address]
-    @latitude = data[:restaurants][0][:restaurant][:location][:latitude] # not sure if i need this here
-    @longitude = data[:restaurants][0][:restaurant][:location][:longitude] # not sure if i need this here
   end
 end
