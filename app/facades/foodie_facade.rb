87e087d086549c9f02c4594082d638e90b2be204
Austin Aspaas <araspaas@live.com>
2021-01-20 09:53:58 -0700

    Update foodie facade

--- app/facades/foodie_facade.rb
+++ app/facades/foodie_facade.rb
@@ -1,6 +1,8 @@
 class FoodieFacade
 
   def self.fetch_data(start, destination, search)
-    
+    restaurant_data = FoodieService.fetch_data(start, destination, search)
+    restaurant = Restaurant.new(restaurant_data)
+    binding.pry
   end
 end
