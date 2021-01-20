e5072f1e0f21f1a1fd89770ca5f57de7346464a1
Austin Aspaas <araspaas@live.com>
2021-01-20 05:42:51 -0700

    Update Background Service

--- app/services/background_service.rb
+++ app/services/background_service.rb
@@ -1,16 +1,16 @@
 class BackgroundService
 
-  def get_background(destination)
+  def self.fetch_data(destination)
     response = conn.get("search/photos") do |req|
-      req.headers['Accept-Version'] = 'v1'
       req.params[:query] = "#{destination} AND landscape"
       req.params[:orientation] = "landscape"
+      req.params[:per_page] = 1
       req.params[:client_id] = ENV["UNSPLASH_CLIENT_ID"]
     end
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def conn
+  def self.conn
     Faraday.new("https://api.unsplash.com/")
   end
 end
