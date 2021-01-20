b8ecdead864c1e8290d9094c09afb1bfd427695c
Austin Aspaas <araspaas@live.com>
2021-01-20 05:37:38 -0700

    Delete obsolete files

--- spec/poros/background_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe Background, :vcr do
-  it "exists" do
-    bg = Background.new("denver,co")
-    expect(bg).to be_a(Background)
-    expect(bg.image_url).to be_a(Array)
-  end
-end
