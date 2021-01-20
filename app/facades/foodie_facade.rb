8293bb8e72e6bbc241ffc120e182a9bf2469d6ff
Austin Aspaas <araspaas@live.com>
2021-01-20 11:39:00 -0700

    Update foodie facade to get info in correct order

--- app/facades/foodie_facade.rb
+++ app/facades/foodie_facade.rb
@@ -1,11 +1,13 @@
 class FoodieFacade
 
   def self.fetch_data(start, destination, search)
-    restaurant_data = FoodieService.fetch_data(start, destination, search)
+    latitude_longitude = LocationService.fetch_data(destination)
+    restaurant_data = FoodieService.fetch_data(latitude_longitude, search)
     restaurant = Restaurant.new(restaurant_data)
     travel_data = LocationService.fetch_road_trip_data(start, destination)
     destination = Destination.new(travel_data, destination)
-    latitude_longitude = LocationService.fetch_data(destination)
-    binding.pry
+    weather_data = WeatherService.fetch_data(latitude_longitude)
+    destination_forecast = DestinationForecast.new(weather_data[:hourly])
+    Foodie.new(restaurant, destination, destination_forecast)
   end
 end
