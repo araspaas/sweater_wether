763d8c614fbb86cdd6d808248ee40e700c10193d
Austin Aspaas <araspaas@live.com>
2021-01-20 11:37:14 -0700

    Fix foodie service to use correct params

--- app/services/foodie_service.rb
+++ app/services/foodie_service.rb
@@ -1,11 +1,12 @@
 class FoodieService
 
-  def self.fetch_data(start, destination, search)
+  def self.fetch_data(latitude_longitude, search)
     response = conn.get("api/v2.1/search") do |req|
       req.headers['user-key'] = ENV["ZAMOTO_API_KEY"]
-      req.params[:search] = search
-      req.params[:start] = start
-      req.params[:end] = destination
+      req.params[:q] = search
+      req.params[:lat] = latitude_longitude[0]
+      req.params[:lon] = latitude_longitude[1]
+      req.params[:count] = 1
     end
     x = JSON.parse(response.body, symbolize_names: true)
   end
