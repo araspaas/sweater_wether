63625de5a702e96f1967da471b499ed4aaf9945b
Austin Aspaas <araspaas@live.com>
2021-01-19 19:51:26 -0700

    Edit background poro

--- app/poros/background.rb
+++ app/poros/background.rb
@@ -1,12 +1,10 @@
 class Background
   attr_reader :location,
-              :image_url,
-              :fake_id
+              :image_url
 
   def initialize(location)
     @location = location
     @image_url = [destination_image[:results][0][:urls][:full]]
-    @fake_id = nil
   end
 
   def destination_image
