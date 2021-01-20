eb04264c95f0e5c5372947a38d97520949108b9d
Austin Aspaas <araspaas@live.com>
2021-01-20 07:49:09 -0700

    remove variable for testing purposes. create fetch_road_trip_data method

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -9,8 +9,19 @@ class LocationService
       req.params[:thumbMaps] = 'false'
     end
     json = JSON.parse(response.body, symbolize_names: true)
-     x = [(json[:results][0][:locations][0][:latLng][:lat]).to_f, (json[:results][0][:locations][0][:latLng][:lng]).to_f]
+    [(json[:results][0][:locations][0][:latLng][:lat]).to_f, (json[:results][0][:locations][0][:latLng][:lng]).to_f]
   end
+
+  def self.fetch_road_trip_data(origin, destination)
+    response = conn.get("/directions/v2/route") do |req|
+      req.params[:key] = ENV["MAPQUEST_API_KEY"]
+      req.params[:to] = destination
+      req.params[:from] = origin
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
 
   def self.conn
     Faraday.new("http://www.mapquestapi.com/")
