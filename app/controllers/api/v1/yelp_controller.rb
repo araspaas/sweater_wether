a48a8fd181cdd2fa3d6da318c74dc97f4a2b69e4
Austin Aspaas <araspaas@live.com>
2021-01-19 19:50:06 -0700

    Delete obsolete files

--- app/controllers/api/v1/yelp_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::YelpController < ApplicationController
-
-  def index
-    destination = Destination.new(params[:start])
-    yelp = Yelp.new(destination.location, destination.latitude, destination.longitude)
-    render json: YelpSerializer.new(yelp)
-  end
-end
--- app/serializers/yelp_serializer.rb
+++
@@ -1,5 +0,0 @@
-class YelpSerializer
-  include FastJsonapi::ObjectSerializer
-  set_id :fake_id
-  attributes :destination_city, :travel_time, :forecast, :restaraunt
-end
