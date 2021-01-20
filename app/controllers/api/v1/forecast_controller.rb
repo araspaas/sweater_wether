2bb9eb8795edf991ddc1ffdfd3194325ebd9650d
Austin Aspaas <araspaas@live.com>
2021-01-19 19:39:17 -0700

    Refactor forecast controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,8 +1,5 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    binding.pry
-    destination = Destination.new(params[:location])
-    forecast = Forecast.new(destination.location, destination.latitude, destination.longitude)
-    render json: ForecastSerializer.new(forecast)
+    render json: ForecastSerializer.new(ForecastFacade.fetch_data(params[:location]))
   end
 end
